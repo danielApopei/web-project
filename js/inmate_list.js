d13381e33364003fa89f3d526633d3185c5791dd
andrei <ochian_a@yahoo.ro>
2024-04-12 19:48:02 +0300

    feat: admin inmate list

---
+++ js/inmate_list.js
@@ -0,0 +1,38 @@
+let inmateList = ["James Wilkinson", "Kelly Pickle", "Ronald Kenney", "Alberto Comeau", "Pamela Grigg", "Paul Garza", "Carmen Myers"];
+let inmateGrid = null;
+let searchBox = null;
+let filteredList = inmateList;
+
+function loadInmateList() {
+    inmateGrid.innerHTML = "";
+    filteredList.sort();
+    if(filteredList.length == 0) {
+        inmateGrid.innerHTML = "No matching results!";
+        return;
+    }
+    for(let i = 0; i < filteredList.length; i++) {
+        let inmateLink = document.createElement("a");
+        inmateLink.textContent = filteredList[i];
+        inmateLink.classList.add("inmate-item");
+        inmateGrid.appendChild(inmateLink);
+    }
+}
+
+function setupSearchBox() {
+    searchBox.addEventListener("input", () => {
+        filteredList = inmateList.filter((name) => {
+            return name.toLowerCase().includes(searchBox.value.toLowerCase());
+        });
+        loadInmateList();
+    });
+}
+
+function setupStuff() {
+    inmateGrid = document.getElementById("inmate-grid");
+    searchBox = document.getElementById("search-box");
+    loadInmateList();
+    setupSearchBox();
+}
+
+document.addEventListener("readystatechange", setupStuff);
+
