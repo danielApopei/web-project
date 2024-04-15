76f873f629452ca0088412d545228174d1b05cb5
danielApopei <danielapopei6@gmail.com>
2024-04-15 22:39:16 +0300

    feat: add links to sidebar

--- js/sidebar.js
+++ js/sidebar.js
@@ -28,42 +28,42 @@ function main() {
 </div>
 <ul>
     <li>
-        <a href="#">
+        <a href="./register_visit.html">
             <i class='bx bxs-pencil' style='color:#86c2f2'></i>
             <span class="nav-item">Plan a visit</span>
         </a>
         <span class="tooltip">Plan a visit</span>
     </li>
     <li>
-        <a href="#">
+        <a href="./admin_visit_list.html">
             <i class='bx bx-list-ul' style='color:#86c2f2'></i>
             <span class="nav-item">Visits list</span>
         </a>
         <span class="tooltip">Visits list</span>
     </li>
     <li>
-        <a href="#">
+        <a href="./admin_inmate_list.html">
             <i class='bx bx-show-alt' style="color: #86c2f2;"></i>
             <span class="nav-item">Prisoners</span>
         </a>
         <span class="tooltip">Prisoners</span>
     </li>
     <li>
-        <a href="#">
+        <a href="./admin_login.html">
             <i class='bx bx-log-in' style='color:#86c2f2'></i>
             <span class="nav-item">LogIn</span>
         </a>
         <span class="tooltip">LogIn</span>
     </li>
     <li>
-        <a href="#">
+        <a href="./admin_register.html">
             <i class="bx bxs-registered" style='color:#86c2f2' ></i>
             <span class="nav-item">Create admin account</span>
         </a>
         <span class="tooltip">Sign in</span>
     </li>
     <li>
-        <a href="#">
+        <a href="./index.html">
             <i class="bx bx-log-out" style='color:#86c2f2'></i>
             <span class="nav-item">Logout</span>
         </a>
