d121713e7d8fe2a68b906e9fd3d3915ac7fe892a
OchianIulian <ochian_a@yahoo.ro>
2024-05-18 13:44:27 +0300

    feat: make integration with backend at register a visit

---
+++ frontend/js/schedule_visit.js
@@ -0,0 +1,40 @@
+const form = document.getElementById('visitForm');
+
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    let visitorName = document.getElementById('visitorName').value;
+    let inmateName = document.getElementById('inmateName').value;
+    let visitorEmail = document.getElementById('visitorEmail').value;
+    let visitorPhone = document.getElementById('visitorPhone').value;
+    let visitDate = document.getElementById('visitDate').value;
+    let visitDuration = document.getElementById('visitDuration').value;
+    let natureOfVisit = document.getElementById('natureOfVisit').value;
+    let relationship = document.getElementById('relationship').value;
+
+
+    let visitData = {
+        visitorName: visitorName,
+        inmateName: inmateName,
+        visitorEmail: visitorEmail,
+        visitorPhone: visitorPhone,
+        visitDate: visitDate,
+        visitDuration: visitDuration,
+        natureOfVisit: natureOfVisit,
+        relationship: relationship
+    };
+
+    fetch('http://localhost:5000/api/visits', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(visitData)
+    }).then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.log('Error: ', error)
+    )
+
+
+    console.log('Form submitted');
+});
